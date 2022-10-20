2972f9c8904c83af44cb11a79cbc71e7c492256a
Areck83 <ntralfre@hotmail.com>
2022-10-20 13:00:34 -0500

    form con spark java y gson

--- practica06/src/main/java/mx/uv/App.java
+++ practica06/src/main/java/mx/uv/App.java
@@ -1,4 +1,8 @@
 package mx.uv;
+import static spark.Spark.*;
+
+import com.google.gson.JsonObject;
+
 
 /**
  * Hello world!
@@ -9,5 +13,20 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/hello", (req, res) -> "Hello World");
+        get("/eduardo", (req, res) -> "Eduardo");
+        get("/alex", (req, res) -> "Alex");
+        get("/brandon", (req, res) -> "Hello brandon");
+        get("/", (req, res) -> "<h1>Bienvenido</h1><img src='https://www.uv.mx/v2/images/logouv.jpg'> ");
+
+        post("/", (req,res)-> {
+            System.out.println(req.queryParams("email") + " " + req.queryParams("password"));
+            System.out.println(req.body());
+            res.status(200);
+            JsonObject oRespuesta = new JsonObject();
+            oRespuesta.addProperty("msj","hola");
+            oRespuesta.addProperty("email", req.queryParams("email"));
+            return oRespuesta;
+        });
     }
 }
